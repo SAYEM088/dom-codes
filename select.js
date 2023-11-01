65c2aab6c1e784e908a2d358c4794cba4bc7f4ca
SAYEM088 <abusayemsarker088@gmail.com>
2023-11-01 13:00:52 +0600

    Dom manipulation old codes practice-2

--- select.js
+++ select.js
@@ -5,4 +5,26 @@ document.getElementById('main-heading')
 // after that it has to store in a variable so that it can access
 
 const heading = document.getElementById('main-heading')
-console.log(heading)
+
+
+// 2. styling an elements
+
+heading.style.color ='red';
+
+const listItems = document.querySelectorAll('.list-items');
+for(i=0;i<listItems.length;i++){
+    listItems[i].style.fontSize = '2rem';
+}
+
+// create an elements
+const ul = document.querySelector('ul');
+const li = document.createElement('li')
+
+// adding Elements
+ul.append(li);
+
+// modifying the text
+const firstListItem =document.querySelector('.list-items');
+console.log(firstListItem.innerHTML);
+console.log(firstListItem.innerText);
+console.log(firstListItem.textContent);
