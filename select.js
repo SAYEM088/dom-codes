a876bbf5bf40ecbfba4744f053f20eaf5e5f39f5
SAYEM088 <abusayemsarker088@gmail.com>
2023-11-01 11:55:51 +0600

    Dom manipulation old codes practice-1

---
+++ select.js
@@ -0,0 +1,8 @@
+// 1.selecting element in DOM by using getElementById...getElementsByClass... etc
+document.getElementById('main-heading')
+
+
+// after that it has to store in a variable so that it can access
+
+const heading = document.getElementById('main-heading')
+console.log(heading)
